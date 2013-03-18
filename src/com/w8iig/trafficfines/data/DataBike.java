d2933f5f6f71bfba45e3ab033deb67101798d188
Dao Hoang Son <daohoangson@gmail.com>
2013-03-18 10:19:25 +0700

    add generated data from scripts/data_parse.py

---
+++ src/com/w8iig/trafficfines/data/DataBike.java
@@ -0,0 +1,129 @@
+package com.w8iig.trafficfines.data;
+
+import com.w8iig.trafficfines.R;
+
+public class DataBike extends DataAbstract {
+
+	@Override
+	public int getTitleResId() {
+		return R.string.data_bike_title;
+	}
+
+	@Override
+	public int[] getNameResIds() {
+		return new int[] {
+			R.string.data_bike_43_2010_ndcp_12_1_a_name,
+			R.string.data_bike_43_2010_ndcp_12_1_b_name,
+			R.string.data_bike_43_2010_ndcp_12_1_c_name,
+			R.string.data_bike_43_2010_ndcp_12_2_a_name,
+			R.string.data_bike_43_2010_ndcp_12_2_b_name,
+			R.string.data_bike_43_2010_ndcp_20_1_a_name,
+			R.string.data_bike_71_2012_ndcp_8_1_name,
+			R.string.data_bike_71_2012_ndcp_8_2_a_name,
+			R.string.data_bike_71_2012_ndcp_8_2_b_name,
+			R.string.data_bike_71_2012_ndcp_8_3_a_name,
+			R.string.data_bike_71_2012_ndcp_8_3_b_name,
+			R.string.data_bike_71_2012_ndcp_8_3_c_name,
+			R.string.data_bike_71_2012_ndcp_8_3_d_name,
+			R.string.data_bike_71_2012_ndcp_8_3_dd_name,
+			R.string.data_bike_71_2012_ndcp_8_3_e_name,
+			R.string.data_bike_71_2012_ndcp_8_3_g_name,
+			R.string.data_bike_71_2012_ndcp_8_4_a_name,
+			R.string.data_bike_71_2012_ndcp_8_4_b_name,
+			R.string.data_bike_71_2012_ndcp_8_4_c_name,
+			R.string.data_bike_71_2012_ndcp_8_4_d_name,
+			R.string.data_bike_71_2012_ndcp_8_4_dd_name,
+			R.string.data_bike_71_2012_ndcp_8_4_e_name,
+			R.string.data_bike_71_2012_ndcp_8_4_g_name,
+			R.string.data_bike_71_2012_ndcp_8_5_a_name,
+			R.string.data_bike_71_2012_ndcp_8_5_b_name
+		};
+	}
+
+	@Override
+	public int[] getDescriptionResIds() {
+		return new int[] {
+			R.string.data_bike_43_2010_ndcp_12_1_a_description,
+			R.string.data_bike_43_2010_ndcp_12_1_b_description,
+			R.string.data_bike_43_2010_ndcp_12_1_c_description,
+			R.string.data_bike_43_2010_ndcp_12_2_a_description,
+			R.string.data_bike_43_2010_ndcp_12_2_b_description,
+			R.string.data_bike_43_2010_ndcp_20_1_a_description,
+			R.string.data_bike_71_2012_ndcp_8_1_description,
+			R.string.data_bike_71_2012_ndcp_8_2_a_description,
+			R.string.data_bike_71_2012_ndcp_8_2_b_description,
+			R.string.data_bike_71_2012_ndcp_8_3_a_description,
+			R.string.data_bike_71_2012_ndcp_8_3_b_description,
+			R.string.data_bike_71_2012_ndcp_8_3_c_description,
+			R.string.data_bike_71_2012_ndcp_8_3_d_description,
+			R.string.data_bike_71_2012_ndcp_8_3_dd_description,
+			R.string.data_bike_71_2012_ndcp_8_3_e_description,
+			R.string.data_bike_71_2012_ndcp_8_3_g_description,
+			R.string.data_bike_71_2012_ndcp_8_4_a_description,
+			R.string.data_bike_71_2012_ndcp_8_4_b_description,
+			R.string.data_bike_71_2012_ndcp_8_4_c_description,
+			R.string.data_bike_71_2012_ndcp_8_4_d_description,
+			R.string.data_bike_71_2012_ndcp_8_4_dd_description,
+			R.string.data_bike_71_2012_ndcp_8_4_e_description,
+			R.string.data_bike_71_2012_ndcp_8_4_g_description,
+			R.string.data_bike_71_2012_ndcp_8_5_a_description,
+			R.string.data_bike_71_2012_ndcp_8_5_b_description
+		};
+	}
+
+	@Override
+	public int[] getValuePairs() {
+		return new int[] {
+			// 43_2010_ndcp_12_1_a
+			40000, 60000,
+			// 43_2010_ndcp_12_1_b
+			40000, 60000,
+			// 43_2010_ndcp_12_1_c
+			40000, 60000,
+			// 43_2010_ndcp_12_2_a
+			60000, 80000,
+			// 43_2010_ndcp_12_2_b
+			60000, 80000,
+			// 43_2010_ndcp_20_1_a
+			80000, 100000,
+			// 71_2012_ndcp_8_1
+			100000, 200000,
+			// 71_2012_ndcp_8_2_a
+			300000, 500000,
+			// 71_2012_ndcp_8_2_b
+			300000, 500000,
+			// 71_2012_ndcp_8_3_a
+			800000, 1200000,
+			// 71_2012_ndcp_8_3_b
+			800000, 1200000,
+			// 71_2012_ndcp_8_3_c
+			800000, 1200000,
+			// 71_2012_ndcp_8_3_d
+			800000, 1200000,
+			// 71_2012_ndcp_8_3_dd
+			800000, 1200000,
+			// 71_2012_ndcp_8_3_e
+			800000, 1200000,
+			// 71_2012_ndcp_8_3_g
+			800000, 1200000,
+			// 71_2012_ndcp_8_4_a
+			2000000, 4000000,
+			// 71_2012_ndcp_8_4_b
+			2000000, 4000000,
+			// 71_2012_ndcp_8_4_c
+			2000000, 4000000,
+			// 71_2012_ndcp_8_4_d
+			2000000, 4000000,
+			// 71_2012_ndcp_8_4_dd
+			2000000, 4000000,
+			// 71_2012_ndcp_8_4_e
+			2000000, 4000000,
+			// 71_2012_ndcp_8_4_g
+			2000000, 4000000,
+			// 71_2012_ndcp_8_5_a
+			4000000, 6000000,
+			// 71_2012_ndcp_8_5_b
+			4000000, 6000000
+		};
+	}
+}
